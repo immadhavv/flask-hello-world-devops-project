8a8ff539469426e7dba15fb3ca91a59ec490ea0a
Niranjan Madhavan D <madhd@cisco.com>
2022-05-09 11:11:41 +0530

    installed dependencies and made minor tweaks

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!!!"
+    return "CI CD Pipeline!!!"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
